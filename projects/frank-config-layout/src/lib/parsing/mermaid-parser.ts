44a1ada1cf6f952588493d5524426ece48d949e9
Vivy <4380412+Matthbo@users.noreply.github.com>
2026-02-25 17:25:29 +0100

    remove linkStyle & classDef checks

--- projects/frank-config-layout/src/lib/parsing/mermaid-parser.ts
+++ projects/frank-config-layout/src/lib/parsing/mermaid-parser.ts
@@ -1,5 +1,5 @@
 /*
-   Copyright 2024-2025 WeAreFrank!
+   Copyright 2024-2026 WeAreFrank!
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -42,9 +42,7 @@ export function getGraphFromMermaid(str: string, d: NodeTextDimensions): Mermaid
   const result = new Graph<MermaidNode, MermaidEdge>();
   const lines: string[] = str.split(/\r?\n/).map((line) => line.trim());
   const nodeLines: string[] = lines.filter((line) => line.search(/^[\dA-Za-z-]+\(/) === 0);
-  const forwardLines: string[] = lines.filter(
-    (line) => !(line.startsWith('classDef') || line.startsWith('linkStyle')) && line.search(/^[\dA-Za-z-]+ /) !== -1,
-  );
+  const forwardLines: string[] = lines.filter((line) => line.search(/^[\dA-Za-z-]+ /) !== -1);
   for (const nodeLine of nodeLines) {
     const id = nodeLine.slice(0, nodeLine.indexOf('('));
     const text = nodeLine.slice(nodeLine.indexOf('(') + 2, nodeLine.lastIndexOf(')') - 1);
